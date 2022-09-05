bd8e0c9001bf1f31192268ee61bf5280a24e85a0
git@github.com:ashrth/SmartContract.git <aashrrita@gmail.com>
2022-09-05 11:52:27 +0530

    out folder created (static)

---
+++ out/_next/static/xuSBWdKl9qJZukkYjgXm-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-ad6d261b06a23fea.js"],"/_error":["static/chunks/pages/_error-f39b03beebab059d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
